a64ba76e73401892c35353abb9654b6cd0a90998
farukphero <farukk6319@gmail.com>
2022-11-08 15:02:15 +0600

    server side start

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port =process.env.PORT || 5000
+
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to fly-plane for better tour')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
+
+ 
